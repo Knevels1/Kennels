5a667c63bff9fe2e20878400e782541680f44e55
Knevels1 <kyle.a.nevels@icloud.com>
2020-12-02 20:59:36 -0600

    got the location and owner to show up on the animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,38 +5,56 @@ import { AnimalProvider } from "./animal/AnimalProvider"
 import { CustomerProvider } from "./customer/CustomerProvider"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import { LocationList } from "./location/LocationList"
-import { AnimalsList } from "./animal/AnimalList"
+import { AnimalList } from "./animal/AnimalList"
 import { CustomersList } from "./customer/CustomerList"
 import { EmployeesList } from "./employee/EmployeeList"
 
 export const ApplicationViews = (props) => {
     return (
         <>
-            <LocationProvider>
+<AnimalProvider>
+    <LocationProvider>
+        <CustomerProvider>
                 {/* Render the location list when http://localhost:3000/ */}
                 <Route exact path="/">
                     <LocationList />
-                </Route>
-            </LocationProvider>
+                    </Route>
+        </CustomerProvider>
+    </LocationProvider>
+</AnimalProvider>
 
-            <AnimalProvider>
+<AnimalProvider>
+    <LocationProvider>
+        <CustomerProvider>
                 {/* Render the animal list when http://localhost:3000/animals */}
                 <Route path="/animals">
-                    <AnimalsList />
+                    <AnimalList />
                 </Route>
-            </AnimalProvider>
-            <CustomerProvider>
+                </CustomerProvider>
+    </LocationProvider>
+</AnimalProvider>
+<AnimalProvider>
+    <LocationProvider>
+        <CustomerProvider>
                 {/* Render the customer list when http://localhost:3000/animals */}
                 <Route path="/customers">
                     <CustomersList />
                 </Route>
-            </CustomerProvider>
+                </CustomerProvider>
+    </LocationProvider>
+</AnimalProvider>
+<AnimalProvider>
+    <LocationProvider>
+        <CustomerProvider>
             <EmployeeProvider>
                 {/* Render the employee list when http://localhost:3000/animals */}
                 <Route path="/employees">
                     <EmployeesList />
                 </Route>
-            </EmployeeProvider>
+                    </EmployeeProvider>
+                </CustomerProvider>
+    </LocationProvider>
+</AnimalProvider>
         </>
     )
 }
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,29 +1,34 @@
 import React, { useContext, useEffect } from "react"
-import { Animal } from "./Animal"
 import { AnimalContext } from "./AnimalProvider"
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
+import { Animal } from "./Animal"
+import "./Animal.css"
 
-export const AnimalsList = () => {
-  // This state changes when `getLocations()` is invoked below
-  const { animals, getAnimals } = useContext(AnimalContext)
+export const AnimalList = () => {
+    const { animals, getAnimals } = useContext(AnimalContext)
+    const { locations, getLocations } = useContext(LocationContext)
+    const { customers, getCustomers } = useContext(CustomerContext)
 
-  /*
-        What's the effect this is reponding to? Component was
-        "mounted" to the DOM. React renders blank HTML first,
-        then gets the data, then re-renders.
-    */
-  useEffect(
-    () => {
-      console.log("AnimalsList: Initial render before data")
-      getAnimals()
-    },
-    []
-  )
+    useEffect(() => {
+        console.log("AnimalList: Initial render before data")
+        getLocations()
+        .then(getCustomers)
+        .then(getAnimals)
+    }, [])
 
-  return (
-    <div className="animals">
-      {
-        animals.map(ani => <Animal key={ani.id} animal={ani} />)
-      }
-    </div>
-  )
+
+    return (
+        <div className="animals">
+            {animals.map(animal => {
+    const owner = customers.find(c => c.id === animal.customerId)
+    const clinic = locations.find(l => l.id === animal.locationId)
+
+    return <Animal key={animal.id}
+                location={clinic}
+                customer={owner}
+                animal={animal} />
+})}
+        </div>
+    )
 }
